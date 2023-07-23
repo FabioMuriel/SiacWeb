2aeacfee2539e265eecc077b454b25c316ca7422
Fabio Muriel <fabiomurielvega16@gmail.com>
2023-07-22 20:12:07 -0500

    Ejemplo de header

--- app/view/home/index.php
+++ app/view/home/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <header>Aca va la informacion del header</header>
     <a href="salir">cerrar Sesion</a>
 </head>
 <body>
